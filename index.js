faaf3a39f791272ce50eda3071c716b21e0fe3ca
dy <df.creative@gmail.com>
2020-01-31 20:18:14 -0500

    Ignore require

--- index.js
+++ index.js
@@ -20,19 +20,21 @@ let hooks
 
 // require is the only way to detect installed hooks
 // until `await import()` or `import.meta.resolve()` is implemented
-if (!hooks) try { hooks = require('react') } catch (e) {}
-if (!hooks) try { hooks = require('preact/hooks') } catch (e) { }
-if (!hooks) try { hooks = require('rax') } catch (e) { }
-if (!hooks) try { hooks = require('haunted') } catch (e) { }
-if (!hooks) try { hooks = require('atomico') } catch (e) { }
-if (!hooks) try { hooks = require('spect') } catch (e) { }
-if (!hooks) try { hooks = require('neverland') } catch (e) { }
-if (!hooks) try { hooks = require('dom-augmentor') } catch (e) { }
-if (!hooks) try { hooks = require('component-register-hooks') } catch (e) { }
-if (!hooks) try { hooks = require('plumejs') } catch (e) { }
-if (!hooks) try { hooks = require('augmentor') } catch (e) { }
-if (!hooks) try { hooks = require('fuco') } catch (e) { }
-if (!hooks) try { hooks = require('tng-hooks') } catch (e) { }
-if (!hooks) try { hooks = require('fn-with-hooks') } catch (e) { }
+if (typeof require !== 'undefined') {
+  if (!hooks) try { hooks = require('react') } catch (e) {}
+  if (!hooks) try { hooks = require('preact/hooks') } catch (e) { }
+  if (!hooks) try { hooks = require('rax') } catch (e) { }
+  if (!hooks) try { hooks = require('haunted') } catch (e) { }
+  if (!hooks) try { hooks = require('atomico') } catch (e) { }
+  if (!hooks) try { hooks = require('spect') } catch (e) { }
+  if (!hooks) try { hooks = require('neverland') } catch (e) { }
+  if (!hooks) try { hooks = require('dom-augmentor') } catch (e) { }
+  if (!hooks) try { hooks = require('component-register-hooks') } catch (e) { }
+  if (!hooks) try { hooks = require('plumejs') } catch (e) { }
+  if (!hooks) try { hooks = require('augmentor') } catch (e) { }
+  if (!hooks) try { hooks = require('fuco') } catch (e) { }
+  if (!hooks) try { hooks = require('tng-hooks') } catch (e) { }
+  if (!hooks) try { hooks = require('fn-with-hooks') } catch (e) { }
+}
 
 if (hooks) register(hooks)
